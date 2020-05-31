86c3503f7ea9bfd0833e631b12946aabcbe6b160
Xiang <chenxiang00825@gmail.com>
2020-05-31 11:09:44 -0400

    src/PointLight.cpp

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -1,9 +1,10 @@
 #include "PointLight.h"
+#include <cmath>
 
 void PointLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+    d = p - q;
+    max_t = sqrt(pow(d[0], 2.0) + pow(d[1], 2.0) + pow(d[2], 2.0));
+    d = d.normalized();
 }
