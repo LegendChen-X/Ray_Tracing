2da4bbb3f3eba4f20957114d30b689f03455a0cb
Xiang <chenxiang00825@gmail.com>
2020-05-31 10:54:11 -0400

    Ts

--- src/TriangleSoup.cpp
+++ src/TriangleSoup.cpp
@@ -1,11 +1,14 @@
 #include "TriangleSoup.h"
-#include "Triangle.h"
+#include "Ray.h"
+// Hint
 #include "first_hit.h"
+
 bool TriangleSoup::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+    int hit = -1;
+    return first_hit(ray,min_t,triangles,hit,t,n);
 }
+
+
+
