d5d7ec8a839d3c7bbb720a844f412b981d65b8fe
Xiang <chenxiang00825@gmail.com>
2020-05-31 11:24:28 -0400

    DirectionalLight

--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -4,8 +4,8 @@
 void DirectionalLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+    max_t = max_t = (double)0xFFFFFFFFFFFFFFFF;
+    
+    d = (-this->d).normalized;
 }
 
